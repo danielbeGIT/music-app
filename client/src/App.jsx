cccdd84c923126b9541d27dbd9bf529542cd4c47
Daniel <Daniel_Be_95@msn.com>
2023-01-07 05:14:30 +0900

    new custom authenticator hook, need fixes not working yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import './assets/styles/App.scss';
 import Login from './components/Login'
+import MusicPage from './components/MusicPage'
+
+const code = new URLSearchParams(window.location.search).get('code')
 
 const App = () => {
-	return (
-		<Login/>
+	return ( 
+		// <>
+		// 	<Login/>
+
+		// 	{code === true && (
+		// 		<MusicPage code={code} />
+		// 	)}
+		// </>
+		code ? <MusicPage code={code} /> : <Login/>
 	)
 }
 
