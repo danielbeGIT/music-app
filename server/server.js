e1100e31ac772622fbee4cbff05b4d260c77a8ce
Daniel <Daniel_Be_95@msn.com>
2023-01-10 05:45:31 +0900

    implementation of server side authenticator, still getting 400 error need fix

--- server/server.js
+++ server/server.js
@@ -1,19 +1,35 @@
+require('dotenv').config
 const express = require('express')
 const SpotifyWebApi = require('spotify-web-api-node')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
 // Refresh Token
 app.post('/refresh', (req, res) => {
-    // console.log("testing")
-
     const refreshToken = req.body.refreshToken
+
+    console.log("testing the token", refreshToken)
+    
     const spotifyApi = new SpotifyWebApi({
-        clientId: "28cf7c00dd294d4ba53c0456903dfb13",
         redirectUri: "http://localhost:3000/",
+        clientId: "28cf7c00dd294d4ba53c0456903dfb13",
         clientSecret: "74670eb768fb4020afdcaaa5c89a23ee",
         refreshToken,
     })
+    spotifyApi.refreshAccessToken().then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+            console.log('Token has been refreshed!', data.body)
+        }).catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
 
@@ -21,10 +37,22 @@ app.post('/refresh', (req, res) => {
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-        clientId: "28cf7c00dd294d4ba53c0456903dfb13",
         redirectUri: "http://localhost:3000/",
+        clientId: "28cf7c00dd294d4ba53c0456903dfb13",
         clientSecret: "74670eb768fb4020afdcaaa5c89a23ee",
     })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in,
+                refreshToken: data.body.refresh_token,
+            })
+            console.log('Token has been received!', data.body)
+        }).catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
 app.listen(3001)
