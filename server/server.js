b9bc14a25952e915ecddadbfeca1da6405c3ae76
Daniel <Daniel_Be_95@msn.com>
2023-01-11 03:37:06 +0900

    login now works without error

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express')
-const SpotifyWebApi = require('spotify-web-api-node')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
 app.use(cors())
@@ -11,48 +11,43 @@ app.use(bodyParser.json())
 // Refresh Token
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
-
-    console.log('testing the token', refreshToken)
-    
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken,
-    })
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: process.env.REDIRECT_URI,
+		clientId: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		refreshToken,
+	})
     spotifyApi.refreshAccessToken().then(data => {
-            res.json({
-                accessToken: data.body.accessToken,
-                expiresIn: data.body.expiresIn,
-            })
-            console.log('Token has been refreshed!', data.body)
-        }).catch(err => {
-            console.log(err)
-            res.sendStatus(400)
-        })
+		res.json({
+			accessToken: data.body.access_token,
+			expiresIn: data.body.expires_in,
+		})
+	}).catch(err => {
+		console.log(err)
+		res.sendStatus(400)
+	})
 })
 
 
 // Login Token
 app.post('/login', (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    })
+	const code = req.body.code
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: process.env.REDIRECT_URI,
+		clientId: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+	})
 
     spotifyApi.authorizationCodeGrant(code).then(data => {
-            res.json({
-                accessToken: data.body.access_token,
-                expiresIn: data.body.expires_in,
-                refreshToken: data.body.refresh_token,
-            })
-            console.log('Token has been received!', data.body)
-        }).catch(err => {
-            console.log(err)
-            res.sendStatus(400)
-        })
+		res.json({
+		  accessToken: data.body.access_token,
+		  refreshToken: data.body.refresh_token,
+		  expiresIn: data.body.expires_in,
+		})
+	}).catch(err => {
+		console.log(err)
+		res.sendStatus(400)
+	})
 })
 
 app.listen(3001)
