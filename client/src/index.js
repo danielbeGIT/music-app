4ca817c2ab4480d119d96a5d7c9048fb3534a52f
Daniel <Daniel_Be_95@msn.com>
2023-05-04 15:06:09 +0900

    small cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from "react-router-dom"
-import { DataLayer } from './context/DataLayer'
-import reducer, { initialState } from './context/reducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { DataLayer } from "./context/DataLayer";
+import reducer, { initialState } from "./context/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
-		<DataLayer 
-			initialState={initialState} 
-			reducer={reducer}
-		>
-			<App />
-		</DataLayer>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </BrowserRouter>
+);
